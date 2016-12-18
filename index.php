a02784512333a41ab057b34e26f8e0c07cfe0925
Aristeides Stathopoulos <aristath@gmail.com>
2016-12-18 14:13:02 +0200

    more improvements

--- index.php
+++ index.php
@@ -11,7 +11,13 @@
     <div id="page" class="hfeed">
         <?php foreach ( kirki_demo_control_types() as $control_type => $title ) : ?>
             <div class="control-section-demo <?php echo sanitize_key( $control_type ); ?>" id="kirki-<?php echo sanitize_key( $control_type ); ?>">
-                <?php get_template_part( 'templates/' . $control_type ); ?>
+                <?php if ( '' !== locate_template( 'templates/' . $control_type . '.php' ) ) : ?>
+                    <?php get_template_part( 'templates/' . $control_type ); ?>
+                <?php else : ?>
+                    <p class="no-demo-found">
+                        <?php printf( esc_attr__( 'No demo found for the %s control', 'kirki-demo' ), '<code>' . $control_type . '</code>' ); ?>
+                    </p>
+                <?php endif; ?>
             </div>
         <?php endforeach; ?>
     </div>
