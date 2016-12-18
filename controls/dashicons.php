34652b0b2d95361095ea352942136be235c2ff6e
Aristeides Stathopoulos <aristath@gmail.com>
2016-12-18 13:51:06 +0200

    adding more demos

---
+++ controls/dashicons.php
@@ -0,0 +1,46 @@
+<?php 
+
+Kirki::add_field( 'kirki_demo', array(
+    'type'        => 'dashicons',
+	'settings'    => 'dashicons0',
+	'label'       => __( 'Dashicons Control', 'my_textdomain' ),
+    'description' => esc_attr__( 'This control is using postMessage & js_vars', 'kirki-demo' ),
+	'section'     => 'dashicons',
+	'default'     => 'format-status',
+	'priority'    => 10,
+    'transport'   => 'postMessage',
+    'js_vars'     => array(
+        array(
+            'element'       => '.control-section-demo.dashicons .dashicons0 .dashicons',
+            'function'      => 'html',
+            'attr'          => 'class',
+            'value_pattern' => 'dashicons dashicons-$',
+        ),
+    ),
+) );
+
+Kirki::add_field( 'kirki_demo', array(
+    'type'            => 'dashicons',
+	'settings'        => 'dashicons1',
+	'label'           => __( 'Dashicons Control', 'my_textdomain' ),
+    'description'     => esc_attr__( 'This control is using postMessage & js_vars', 'kirki-demo' ),
+	'section'         => 'dashicons',
+	'default'         => 'format-status',
+	'priority'        => 10,
+    'transport'       => 'postMessage',
+    'partial_refresh' => array(
+        'dashicons1' => array(
+            'selector'        => '.control-section-demo.dashicons .dashicons1',
+            'render_callback' => function() {
+                echo '<div class="dashicons1">';
+                $icon1 = get_theme_mod( 'dashicons1', 'format-status' );
+                printf(
+                    esc_attr__( 'Using partial_refresh: %s', 'kirki-demo' ),
+                    '<span class="dashicons dashicons-' . esc_attr( $icon1 ) . '"></span>'
+                );
+                echo '</div>';
+            },
+        ),
+    ),
+) );
+
---
+++ templates/dashicons.php
@@ -0,0 +1,19 @@
+<div class="dashicons0">
+    <?php
+    $icon0 = get_theme_mod( 'dashicons0', 'format-status' );
+    printf(
+        esc_attr__( 'Using jsVars: %s', 'kirki-demo' ),
+        '<span class="dashicons dashicons-' . esc_attr( $icon0 ) . '"></span>'
+    );
+    ?>
+</div>
+
+<div class="dashicons1">
+    <?php
+    $icon1 = get_theme_mod( 'dashicons1', 'format-status' );
+    printf(
+        esc_attr__( 'Using partial_refresh: %s', 'kirki-demo' ),
+        '<span class="dashicons dashicons-' . esc_attr( $icon1 ) . '"></span>'
+    );
+    ?>
+</div>
